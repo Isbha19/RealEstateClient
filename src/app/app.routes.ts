13cc8f1e14b3f6479e788de8374e846d3c1be636
Isbha Khalid <isbhakhalid19@gmail.com>
2024-06-09 05:57:10 +0400

    Set extra files including (material module)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:AppComponent},
+    {path:"not-found",component:NotFoundComponent},
+    {path:'**',component:NotFoundComponent}
+];
